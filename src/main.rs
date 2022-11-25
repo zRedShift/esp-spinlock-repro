d48a97ae303e311f1d2299e5c1e1fc3c9ef89dce
Ronen Ulanovsky <ronen.ulanovsky@nanit.com>
2022-11-25 15:05:25 +0200

    manually inline heapless

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
 #![no_std]
 #![no_main]
 
+use core::ops::IndexMut;
+use core::sync::atomic::{AtomicU8, Ordering};
 use esp32s3_hal::{clock::ClockControl, pac::Peripherals, prelude::*, timer::TimerGroup, Rtc};
 use esp_backtrace as _;
+#[cfg(feature = "esp-println")]
+use esp_println::println;
+
+#[cfg(not(feature = "esp-println"))]
+macro_rules! println {
+    ($($arg:tt)*) => {};
+}
 
 #[xtensa_lx_rt::entry]
 fn main() -> ! {
@@ -21,12 +30,70 @@ fn main() -> ! {
     wdt0.disable();
     wdt1.disable();
 
-    let queue = heapless::mpmc::MpMcQueue::<(), 8>::new();
-    let mut counter = 0;
+    const N: usize = 8;
+    const MASK: u8 = N as u8 - 1;
+    let mut cells: [_; N] = core::array::from_fn(|i| AtomicU8::new(i as u8));
+    let dequeue_pos = AtomicU8::new(0);
+    let enqueue_pos = AtomicU8::new(0);
     loop {
-        esp_println::println!("counter at {counter}");
-        queue.enqueue(()).unwrap();
-        queue.dequeue().unwrap();
-        counter += 1;
+        let mut pos = enqueue_pos.load(Ordering::Relaxed);
+        println!("enqueue pos at {pos}");
+        let mut cell;
+        loop {
+            cell = cells.index_mut((pos & MASK) as usize);
+            let seq = cell.load(Ordering::Acquire);
+            println!("enqueue seq at {seq}");
+            let dif = (seq as i8).wrapping_sub(pos as i8);
+            println!("enqueue dif at {seq}");
+            assert!(dif >= 0);
+            if dif == 0 {
+                if enqueue_pos
+                    .compare_exchange_weak(
+                        pos,
+                        pos.wrapping_add(1),
+                        Ordering::Relaxed,
+                        Ordering::Relaxed,
+                    )
+                    .is_ok()
+                {
+                    break;
+                }
+            } else {
+                pos = enqueue_pos.load(Ordering::Relaxed);
+                println!("enqueue pos at {pos}");
+            }
+        }
+        let new_seq = pos.wrapping_add(1);
+        println!("enqueue new seq at {new_seq}");
+        cell.store(new_seq, Ordering::Release);
+        pos = dequeue_pos.load(Ordering::Relaxed);
+        println!("dequeue pos at {pos}");
+        loop {
+            cell = cells.index_mut((pos & MASK) as usize);
+            let seq = cell.load(Ordering::Acquire);
+            println!("dequeue seq at {seq}");
+            let dif = (seq as i8).wrapping_sub((pos.wrapping_add(1)) as i8);
+            println!("dequeue dif at {seq}");
+            assert!(dif >= 0);
+            if dif == 0 {
+                if dequeue_pos
+                    .compare_exchange_weak(
+                        pos,
+                        pos.wrapping_add(1),
+                        Ordering::Relaxed,
+                        Ordering::Relaxed,
+                    )
+                    .is_ok()
+                {
+                    break;
+                }
+            } else {
+                pos = dequeue_pos.load(Ordering::Relaxed);
+                println!("dequeue pos at {pos}");
+            }
+        }
+        let new_seq = pos.wrapping_add(MASK).wrapping_add(1);
+        println!("dequeue new seq at {new_seq}");
+        cell.store(new_seq, Ordering::Release);
     }
 }
